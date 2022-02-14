1c96968a3e307527f8696c942047c3db6ffb7878
Yuzunoha <studysteadystandby@gmail.com>
2022-02-14 20:55:01 +0900

    ディレクティブを追加した

--- html/laravelapp/app/Providers/AppServiceProvider.php
+++ html/laravelapp/app/Providers/AppServiceProvider.php
@@ -23,6 +23,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // https://blog.capilano-fw.com/?p=4668
+        // 指定されたテキストからビューを取得し「そのまま」コードを取得します ※
+        // パッケージを使ってMarkdownをHTMLへ変換
+        // 変換したコードを返す
+        Blade::directive('markdown', function ($expression) {
+            $markdown = view(
+                str_replace('\'', '', $expression)
+            )->render();
+            $Parsedown = new \Parsedown();
+            return $Parsedown->text($markdown);
+        });
     }
 }
