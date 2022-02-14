049ffd84ae99d46ef7c975d7ff768ff1a01d6c69
Yuzunoha <studysteadystandby@gmail.com>
2022-02-15 01:00:19 +0900

    やった

--- html/laravelapp/app/Http/Controllers/MarkdowntextController.php
+++ html/laravelapp/app/Http/Controllers/MarkdowntextController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Markdowntext;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Markdown;
 
 class MarkdowntextController extends Controller
 {
@@ -15,6 +16,7 @@ class MarkdowntextController extends Controller
         } else {
             $text = $this->example();
         }
+        $text = Markdown::parse($text);
         return view('markdowntext', compact('text'));
     }
 
@@ -134,46 +136,6 @@ class MarkdowntextController extends Controller
         |Single backticks|`'Isn't this fun?'`            |'Isn't this fun?'            |
         |Quotes          |`"Isn't this fun?"`            |"Isn't this fun?"            |
         |Dashes          |`-- is en-dash, --- is em-dash`|-- is en-dash, --- is em-dash|
-        
-        
-        ## KaTeX
-        
-        You can render LaTeX mathematical expressions using [KaTeX](https://khan.github.io/KaTeX/):
-        
-        The *Gamma function* satisfying $\Gamma(n) = (n-1)!\quad\forall n\in\mathbb N$ is via the Euler integral
-        
-        $$
-        \Gamma(z) = \int_0^\infty t^{z-1}e^{-t}dt\,.
-        $$
-        
-        > You can find more information about **LaTeX** mathematical expressions [here](http://meta.math.stackexchange.com/questions/5020/mathjax-basic-tutorial-and-quick-reference).
-        
-        
-        ## UML diagrams
-        
-        You can render UML diagrams using [Mermaid](https://mermaidjs.github.io/). For example, this will produce a sequence diagram:
-        
-        ```mermaid
-        sequenceDiagram
-        Alice ->> Bob: Hello Bob, how are you?
-        Bob-->>John: How about you John?
-        Bob--x Alice: I am good thanks!
-        Bob-x John: I am good thanks!
-        Note right of John: Bob thinks a long<br/>long time, so long<br/>that the text does<br/>not fit on a row.
-        
-        Bob-->Alice: Checking with John...
-        Alice->John: Yes... John, how are you?
-        ```
-        
-        And this will produce a flow chart:
-        
-        ```mermaid
-        graph LR
-        A[Square Rect] -- Link text --> B((Circle))
-        A --> C(Round Rect)
-        B --> D{Rhombus}
-        C --> D
-        ```
         EOM;
 
         return $t;
