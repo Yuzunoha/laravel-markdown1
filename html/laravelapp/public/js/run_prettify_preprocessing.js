acd9ba376cbe8d15610c27e2c3befdef9cbb203d
Yuzunoha <studysteadystandby@gmail.com>
2022-02-18 23:33:36 +0900

    js完成

--- html/laravelapp/public/js/run_prettify_preprocessing.js
+++ html/laravelapp/public/js/run_prettify_preprocessing.js
@@ -1,11 +1,21 @@
 const p = console.log;
 
+/**
+ * language-PHp -> lang-php
+ * language-js -> lang-js
+ */
+const convertLanguageClass = (str) => {
+  return 'lang-' + str.trim().split('-')[1].toLowerCase();
+};
+
 const main = () => {
   const codes = document.querySelectorAll("[class^='lang']"); // langから始まるクラス
   for (const code of codes) {
+    const newLangClass = convertLanguageClass(code.classList[0]);
     const pre = code.parentNode;
+
     pre.textContent = code.textContent; // codeタグは消える
-    pre.classList.add('prettyprint', 'linenums');
+    pre.classList.add('prettyprint', newLangClass, 'linenums');
   }
 };
 
