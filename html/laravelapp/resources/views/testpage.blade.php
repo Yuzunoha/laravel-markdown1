870ea30af5f0f1d4a978de8a8051ab06684ee5bc
Yuzunoha <studysteadystandby@gmail.com>
2022-02-18 18:45:37 +0900

    読み込めた

--- html/laravelapp/resources/views/testpage.blade.php
+++ html/laravelapp/resources/views/testpage.blade.php
@@ -7,21 +7,41 @@
 <body>
   <div>
     testpage!!!
+    <pre class="prettyprint lang-php">
+class MarkdowntextController extends Controller
+{
+  public function index()
+  {
+    $m = Markdowntext::find(1);
+    if ($m) {
+      $rawtext = $m->text;
+    } else {
+      $rawtext = $this->example();
+    }
+    $markdowntext = Markdown::parse($rawtext);
+    return view('markdowntext', compact('markdowntext', 'rawtext'));
+  }
+
+  public function store(Request $request)
+  {
+    $m = Markdowntext::find(1);
+    if (!$m) {
+      $m = new Markdowntext();
+    }
+    $m->text = $request->textarea1;
+    $m->save();
+    return redirect()->action('MarkdowntextController@index');
+  }
+}
+    </pre>
   </div>
   <pre>
     <code class="prettyprint lang-js">
     console.log('Hello World')
     </code>
   </pre>
-  <script>
-    const elements = document.querySelectorAll("[class^='lang']"); // langから始まるクラス
-    for (const element of elements) {
-      element.classList.add('prettyprint');
-      // element.style.fontSize = '16px';
-      element.style.fontWeight = "bold";
-    }
-  </script>
-  <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+  <script src="{{ asset('/js/monokai-preprocessing.js') }}"></script>
+  <!-- <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script> -->
 </body>
 
 </html>
