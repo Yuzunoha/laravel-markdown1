799bb5a2e44728ac81ba9b3999d33f0f179a634b
Yuzunoha <studysteadystandby@gmail.com>
2022-02-18 13:54:32 +0900

    test

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'MarkdowntextController@index');
 Route::post('/', 'MarkdowntextController@store');
+Route::get('/test', function () {
+  echo '<pre>';
+  echo $_SERVER['HTTP_HOST'];
+});
