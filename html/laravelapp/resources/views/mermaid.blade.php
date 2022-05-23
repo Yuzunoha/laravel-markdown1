ba3952c9e6f83676bdd8db1483c4a6c3013eac58
Yuzunoha <studysteadystandby@gmail.com>
2022-05-23 11:06:01 +0900

    使えた

--- html/laravelapp/resources/views/mermaid.blade.php
+++ html/laravelapp/resources/views/mermaid.blade.php
@@ -1,21 +1,26 @@
-<html>
+<html lang="en">
 
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css">
+    <link href="mermaid.min.css" rel="stylesheet">
+    </link>
 </head>
 
 <body>
-    <div>
-        {{ $markdowntext }}
-        {{ Form::open(['action' => 'MarkdowntextController@store', 'method' => 'post']) }}
-        {{ Form::token() }}
-        {{ Form::textarea('textarea1', $rawtext, ['class' => 'form-control',  'rows' => '80', 'cols' => '120']) }}
-        {{ Form::submit('上書き', ['class' => 'btn btn-success btn-lg']) }}
-        {{ Form::close() }}
+    <div class="mermaid">
+        graph TD;
+        スーパーに入る --> 品物をかごに入れる
+        品物をかごに入れる --> レジに並ぶ
+        レジに並ぶ --> お金を払う
+        お金を払う --> 袋詰する
+        袋詰する --> 帰る
+
     </div>
-    <script src="{{ asset('js/run_prettify_preprocessing.js') }}"></script>
-    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
-    <link rel="stylesheet" href="https://jmblog.github.io/color-themes-for-google-code-prettify/themes/github-v2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+    <script>
+        mermaid.initialize({
+            startOnLoad: true
+        });
+    </script>
 </body>
 
 </html>
