f554c444a23b100c4d8fd1834279f663f32fb528
Yuzunoha <studysteadystandby@gmail.com>
2022-05-23 10:43:35 +0900

    これ不要だった

--- html/laravelapp/app/Providers/AppServiceProvider.php
+++ html/laravelapp/app/Providers/AppServiceProvider.php
@@ -8,41 +8,29 @@ use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
-  /**
-   * Register any application services.
-   *
-   * @return void
-   */
-  public function register()
-  {
-    //
-  }
-
-  /**
-   * Bootstrap any application services.
-   *
-   * @return void
-   */
-  public function boot(UrlGenerator $url)
-  {
-    // https://blog.capilano-fw.com/?p=4668
-    // 指定されたテキストからビューを取得し「そのまま」コードを取得します ※
-    // パッケージを使ってMarkdownをHTMLへ変換
-    // 変換したコードを返す
-    Blade::directive('markdown', function ($expression) {
-      $markdown = view(
-        str_replace('\'', '', $expression)
-      )->render();
-      $Parsedown = new \Parsedown();
-      return $Parsedown->text($markdown);
-    });
-
-    // ssl化(https強制)
-    $keyHttpHost = 'HTTP_HOST';
-    if (array_key_exists($keyHttpHost, $_SERVER)) {
-      if ('localhost' !== $_SERVER[$keyHttpHost]) {
-        $url->forceScheme('https');
-      }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
     }
-  }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(UrlGenerator $url)
+    {
+        // ssl化(https強制)
+        $keyHttpHost = 'HTTP_HOST';
+        if (array_key_exists($keyHttpHost, $_SERVER)) {
+            if ('localhost' !== $_SERVER[$keyHttpHost]) {
+                $url->forceScheme('https');
+            }
+        }
+    }
 }
